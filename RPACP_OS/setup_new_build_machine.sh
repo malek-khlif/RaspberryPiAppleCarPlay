f2fd6f573d375634d6cd1611edffa48db2f3cf9c
Malek Khlif <80583758+malek-khlif@users.noreply.github.com>
2024-02-02 16:25:06 +0100

    feat: finish the implementation of setup_new_build_machine.sh

--- RPACP_OS/setup_new_build_machine.sh
+++ RPACP_OS/setup_new_build_machine.sh
@@ -5,13 +5,14 @@
 # This script should be run as root only once after a fresh installation of Ubuntu 22.04 LTS
 
 # Author: Malek Khlif (malek.khlif@outlook.com)
-# Date: 26 Januray 2024
+# Date: 3 February 2024
 # Version: 1.0
 # License: GPLv3
 
 # Script arguments:
 #   -h: help
-#   -c: clean
+#   -i: install
+#   -u: uninstall
 
 ##############################
 ##############################
@@ -22,6 +23,15 @@
 
 PROJECT_NAME="RPACP_OS"
 PROJECT_VERSION="1.0"
+PROJECT_FULL_NAME="${PROJECT_NAME} v${PROJECT_VERSION}"
+
+COMMAND=""
+
+OS_MACHINE_CONFIGURED_FILE="/etc/RPACP_OS.machine.configured"
+
+INFO_MESSAGE_TYPE=0
+WARNING_MESSAGE_TYPE=1
+ERROR_MESSAGE_TYPE=2
 ################################################################################
 
 ################################################################################
@@ -31,39 +41,49 @@ PROJECT_VERSION="1.0"
 #   This function is used to print a message in a specific format.
 #
 # Parameters:
-#   $1 - message: Message to be printed.
-#   $2 - type: Type of the message. It can be one of the following:
-#       - 0: INFO
-#       - 1: WARNING
-#       - 2: ERROR
+#   $1 - Type of the message. It can be one of the following:
+#       - INFO_MESSAGE_TYPE
+#       - WARNING_MESSAGE_TYPE
+#       - ERROR_MESSAGE_TYPE
+#
+#   $2 - message: Message to be printed.
 #
 # Usage:
-#   printMessage "This is a message" 0
+#   printMessage $INFO_MESSAGE_TYPE "This is an info message"
 ################################################################################
 function printMessage {
-    local message=$1
-    local type=$2
-    local timestamp=$(date +'%H:%M:%S - %d/%m/%Y')
-    
-    local red='\033[0;31m'
-    local green='\033[0;32m'
-    local yellow='\033[0;33m'
-    local NC='\033[0m' # No Color
+    local type=$1
+    local message=$2
+    local timestamp=$(date --iso-8601=seconds)
+
+    local reset_color="\033[0m"
+    local green_color="\033[32m"
+    local yellow_color="\033[33m"
+    local red_color="\033[31m"
+    local color="$reset_color"
+
+    local log_level=""
 
     case $type in
-        0)
-            echo -e "[${PROJECT_NAME}] - [${timestamp}] - ${green}[INFO]${NC} - ${message}"
+        $INFO_MESSAGE_TYPE)
+            color=$green_color
+            log_level="INFO"
             ;;
-        1)
-            echo -e "[${PROJECT_NAME}] - [${timestamp}] - ${yellow}[WARNING]${NC} - ${message}"
+        $WARNING_MESSAGE_TYPE)
+            color=$yellow_color
+            log_level="WARNING"
             ;;
-        2)
-            echo -e "[${PROJECT_NAME}] - [${timestamp}] - ${red}[ERROR]${NC} - ${message}"
+        $ERROR_MESSAGE_TYPE)
+            color=$red_color
+            log_level="ERROR"
             ;;
         *)
-            echo -e "[${PROJECT_NAME}] - [${timestamp}] - ${red}[ERROR]${NC} - ${message}"
+            color=$red_color
+            log_level="ERROR"
             ;;
     esac
+
+    echo -e "[${timestamp}] - [${PROJECT_FULL_NAME}] - ${color}[${log_level}]${reset_color} - ${message}"
 }
 
 ################################################################################
@@ -78,8 +98,10 @@ function printMessage {
 #   print_help
 ################################################################################
 function print_help {
-    printMessage "Usage: ${0} [-h]\n
-    \t-h: help\n" 0
+    printMessage $INFO_MESSAGE_TYPE "Usage: ${0} [-h] [-i] [-u]\n
+    \t-h: help      - Print this help message
+    \t-i: install   - Install the necessary packages to build RPACP_OS
+    \t-u: uninstall - Uninstall the necessary packages to build RPACP_OS"
 }
 
 ################################################################################
@@ -100,24 +122,30 @@ function parse_arguments {
 
     # Check if no arguments were passed
     if [ $# -eq 0 ]; then
-        printMessage "No arguments provided. Please specify options." 2
+        printMessage $ERROR_MESSAGE_TYPE "No arguments provided. Please specify options."
         print_help
         exit 1
     fi    
 
-    while getopts "h" opt; do
+    while getopts "hiu" opt; do
         case ${opt} in
             h )
                 print_help
                 exit 0
                 ;;
+            i )
+                COMMAND="install"
+                ;;
+            u )
+                COMMAND="uninstall"
+                ;;     
             \? )
-                printMessage "Invalid Option: -$OPTARG" 2
+                printMessage $ERROR_MESSAGE_TYPE "Invalid Option: -$OPTARG"
                 print_help
                 exit 1
                 ;;
             : )
-                printMessage "Invalid Option: -$OPTARG requires an argument" 2
+                printMessage $ERROR_MESSAGE_TYPE "Invalid Option: -$OPTARG requires an argument"
                 print_help
                 exit 1
                 ;;
@@ -126,9 +154,77 @@ function parse_arguments {
 }
 
 ################################################################################
+# Function Name: apt_install_necessary_packages
+#
+# Description:
+#   This function is used to install the necessary packages.
+#
+# Parameters:
+#
+# Usage:
+#   apt_install_necessary_packages
+################################################################################
+function apt_install_necessary_packages {
+    #FROM https://docs.yoctoproject.org/brief-yoctoprojectqs/index.html#build-host-packages
+    printMessage $INFO_MESSAGE_TYPE "Installing necessary packages..."
+
+    if apt install -qq -y gcc g++ cmake make gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev python3-subunit mesa-common-dev zstd liblz4-tool file locales libacl1; then
+        printMessage $INFO_MESSAGE_TYPE "Package installation succeeded."
+    else
+        printMessage $ERROR_MESSAGE_TYPE "Package installation failed."
+        exit 1
+    fi
+
+    locale-gen en_US.UTF-8
+
+    touch ${OS_MACHINE_CONFIGURED_FILE}
+}
+
+################################################################################
+# Function Name: apt_uninstall_necessary_packages
+#
+# Description:
+#   This function is used to uninstall the necessary packages.
+#
+# Parameters:
+#
+# Usage:
+#   apt_uninstall_necessary_packages
+################################################################################
+function apt_uninstall_necessary_packages {
+    printMessage $INFO_MESSAGE_TYPE "Uninstalling necessary packages..."
+    rm -f ${OS_MACHINE_CONFIGURED_FILE}
+    printMessage $INFO_MESSAGE_TYPE "Package uninstallation succeeded."
+}
+
+################################################################################
 # Script start
 ################################################################################
 
 # Parse the arguments of the script
 parse_arguments "$@"
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    printMessage $ERROR_MESSAGE_TYPE "Please run as root"
+    exit 1
+fi
+
+# Check if the COMMAND is empty
+if [ -z "${COMMAND}" ]; then
+    printMessage $ERROR_MESSAGE_TYPE "No arguments provided. Please specify options."
+    print_help
+    exit 1
+fi
+
+# If the COMMAND is install
+if [ "${COMMAND}" == "install" ]; then
+    apt_install_necessary_packages
+elif [ "${COMMAND}" == "uninstall" ]; then
+    apt_uninstall_necessary_packages
+else
+    printMessage $ERROR_MESSAGE_TYPE "Invalid command"
+    print_help
+    exit 1    
+fi
 
