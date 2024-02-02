62a2a1e9faff795d975b57f07cfc05cb77b28439
Malek Khlif <80583758+malek-khlif@users.noreply.github.com>
2024-02-02 23:22:44 +0100

    sdf

--- RPACP_OS/build.sh
+++ RPACP_OS/build.sh
@@ -12,3 +12,69 @@
 #   -p, --path <path>: The path to build the operating system. Default is "/tmp/RPACP_OS".
 #   -b, --build <recipe>: The recipe to build. Default is "all".
 #   -c, --clean: Clean the build directory.
+
+################################################################################
+# Global variables
+
+PROJECT_NAME="RPACP_OS"
+PROJECT_VERSION="1.0"
+PROJECT_FULL_NAME="${PROJECT_NAME} v${PROJECT_VERSION}"
+
+OS_MACHINE_CONFIGURED_FILE="/etc/RPACP_OS.machine.configured"
+
+INFO_MESSAGE_TYPE=0
+WARNING_MESSAGE_TYPE=1
+ERROR_MESSAGE_TYPE=2
+################################################################################
+
+################################################################################
+# Function Name: printMessage
+#
+# Description:
+#   This function is used to print a message in a specific format.
+#
+# Parameters:
+#   $1 - Type of the message. It can be one of the following:
+#       - INFO_MESSAGE_TYPE
+#       - WARNING_MESSAGE_TYPE
+#       - ERROR_MESSAGE_TYPE
+#
+#   $2 - message: Message to be printed.
+#
+# Usage:
+#   printMessage $INFO_MESSAGE_TYPE "This is an info message"
+################################################################################
+function printMessage {
+    local type=$1
+    local message=$2
+    local timestamp=$(date --iso-8601=seconds)
+
+    local reset_color="\033[0m"
+    local green_color="\033[32m"
+    local yellow_color="\033[33m"
+    local red_color="\033[31m"
+    local color="$reset_color"
+
+    local log_level=""
+
+    case $type in
+        $INFO_MESSAGE_TYPE)
+            color=$green_color
+            log_level="INFO"
+            ;;
+        $WARNING_MESSAGE_TYPE)
+            color=$yellow_color
+            log_level="WARNING"
+            ;;
+        $ERROR_MESSAGE_TYPE)
+            color=$red_color
+            log_level="ERROR"
+            ;;
+        *)
+            color=$red_color
+            log_level="ERROR"
+            ;;
+    esac
+
+    echo -e "[${timestamp}] - [${PROJECT_FULL_NAME}] - ${color}[${log_level}]${reset_color} - ${message}"
+}
